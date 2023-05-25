2590ff5ba6f6f2705e8b1aa9904734b3398cebde
Elvin <elvindavin@gmail.com>
2023-05-25 19:09:06 -0400

    third commit

--- script.groovy
+++ script.groovy
@@ -5,10 +5,10 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+    withCredentials([usernamePassword(credentialsId: 'dockerid', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t elvindavin4u/practice:v1 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push elvindavin4u/practice:v1'
     }
 } 
 
